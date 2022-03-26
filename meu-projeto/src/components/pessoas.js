7b8888abdfd2ee4d3d2849b0200be995946db228
TiMattos <thiagopmattos@hotmail.com>
2022-03-26 20:34:09 -0300

    subindo aulas

--- meu-projeto/src/components/pessoas.js
+++ meu-projeto/src/components/pessoas.js
@@ -1,8 +1,10 @@
+import styles from './pessoas.module.css'
+
 function Pessoas({nome,idade,profissao,foto}){
     return (
-<div>
+<div className={styles.fraseContainer}>
 <img src={foto} alt={nome}/>
-<h2>Nome: {nome}</h2>
+<h2 className={styles.fraseContent}>Nome: {nome}</h2>
 <p>Idade: {idade}</p>
 <p>Profissão: {profissao}</p>
 </div>
